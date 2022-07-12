c2073eae51b32ea4aa9864ac6b384c46c868e436
Stephen Archer <s.archer@f5.com>
2022-07-12 14:47:57 +0100

    created initial tf config

---
+++ main.tf
@@ -0,0 +1,83 @@
+terraform {
+  required_providers {
+    volterra = {
+      source  = "volterraedge/volterra"
+      version = "0.11.2"
+    }
+    # azurerm = {
+    #   source  = "hashicorp/azurerm"
+    #   version = "=2.46.0"
+    # }
+  }
+}
+
+provider "volterra" {
+  # Configuration options.
+  url          = format("https://%s.console.ves.volterra.io/api", var.TENANT)
+  api_p12_file = var.API_P12_PATH
+}
+
+# provider "azurerm" {
+#   features {}
+# }
+
+resource "volterra_origin_pool" "gcp-origin" {
+  name                   = format("gcp-%s-tf", var.SHORTNAME)
+  namespace              = var.NAMESPACE
+  description            = "Created by Terraform"
+  endpoint_selection     = "LOCAL_PREFERRED"
+  loadbalancer_algorithm = "LB_OVERRIDE"
+
+  port   = var.ORIGIN_PORT
+  no_tls = true
+
+  origin_servers {
+    private_ip {
+      ip              = var.ORIGIN_IP
+      outside_network = true
+      site_locator {
+        site {
+          tenant    = null
+          namespace = "system"
+          name      = var.ORIGIN_SITE
+        }
+      }
+    }
+  }
+}
+
+resource "volterra_http_loadbalancer" "gcp-nginx-lb" {
+  name                   = format("gcp-%s-tf", var.SHORTNAME)
+  namespace              = var.NAMESPACE
+  description            = "Created by Terraform"
+  domains                = [var.DOMAIN_NAME]
+
+  advertise_on_public_default_vip = true
+  no_challenge                    = true
+  round_robin                     = true
+  disable_rate_limit              = true
+  no_service_policies             = true
+  disable_waf                     = true
+  multi_lb_app                    = true
+  user_id_client_ip               = true
+  
+  https_auto_cert {
+    add_hsts       = false
+    http_redirect  = true
+    no_mtls        = true
+    default_header = true
+    disable_path_normalize = true
+
+    tls_config {
+      default_security = true
+    }
+  }
+
+  default_route_pools {
+    pool {
+      name      = volterra_origin_pool.gcp-origin.name
+      namespace = var.NAMESPACE
+    }
+    weight = 1
+  }
+}
---
+++ vars.tfvars
@@ -0,0 +1,8 @@
+API_P12_PATH = "../../creds/galileo.console.ves.volterra.io.api-creds.p12"
+TENANT       = "galileo"
+NAMESPACE    = "arch"
+SHORTNAME    = "nginx-cc1"
+ORIGIN_PORT  = 80
+ORIGIN_IP    = "10.0.0.3"
+ORIGIN_SITE  = "gcp-test-site"
+DOMAIN_NAME  = "testing.vol"
