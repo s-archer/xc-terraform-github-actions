58bf5c0f3ad84a67d69be881dd515e7c5260061a
Stephen Archer <s.archer@f5.com>
2022-07-13 08:45:26 +0100

    added test vars to vars.tf

---
+++ vars.tf
@@ -0,0 +1,33 @@
+variable "TENANT" {
+  type    = string
+  default = "galileo"
+}
+
+variable "API_P12_PATH" {
+  type     = string
+  default  = "/p12.fake"
+}
+
+variable "NAMESPACE" {
+  type = string
+}
+
+variable "SHORTNAME" {
+  type = string
+}
+
+variable "ORIGIN_PORT" {
+  type = string
+}
+
+variable "ORIGIN_IP" {
+  type = string
+}
+
+variable "ORIGIN_SITE" {
+  type = string
+}
+
+variable "DOMAIN_NAME" {
+  type = string
+}
