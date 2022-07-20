b8a41193f1bca16c0c64c8cd73585d2cdeffbff1
Stephen Archer <s.archer@f5.com>
2022-07-21 00:10:24 +0100

    use terraform output for rules

--- waf_exclusion_rules/git_ops.tf
+++ waf_exclusion_rules/git_ops.tf
@@ -1,15 +1,15 @@
-resource "gitops_checkout" "changes_checkout" {
+resource "gitops_checkout" "updates_checkout" {
     path = "./"
-    branch = "changes"
+    branch = "updates"
 }
 
 resource "gitops_file" "waf_file" {
-  checkout = gitops_checkout.changes_checkout.id
+  checkout = gitops_checkout.updates_checkout.id
   path = "waf_exclusion_rules_defined_within_interval_git.tf"
   contents  = format("variable \"waf_exclusion_rules\" {\n  type = set( object( {\n    signature_id = string\n    method = string\n    host = string\n    path = string\n    params = string\n  } ) )\n  default = %s\n}", data.jq_query.json_parser.result)
 }
 
-resource "gitops_commit" "changes_commit" {
+resource "gitops_commit" "updates_commit" {
   commit_message = "Created by terraform gitops_commit"
   handles = [gitops_file.waf_file.id]
 }
