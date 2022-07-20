24a010f95ce5889b927d2be071246a411ffa36de
Stephen Archer <s.archer@f5.com>
2022-07-20 17:47:58 +0100

    updated origin and lb

--- create_waf_exclusion_rules_var_file.tf
+++ create_waf_exclusion_rules_var_file.tf
@@ -1,50 +1,34 @@
-variable start_timestamp {
-  type = string
+variable "start_timestamp" {
+  type    = string
   default = "1658232000"
 }
 
-variable end_timestamp {
-  type = string
+variable "end_timestamp" {
+  type    = string
   default = "1658260800"
 }
-
-terraform {
-  required_providers {
-    http-full = {
-      source = "salrashid123/http-full"
-    }
-    jq = {
-      source  = "massdriver-cloud/jq"
-      version = "0.2.0"
-    }
-  }
-}
-
-provider "http-full" {}
-
-provider "jq" {}
 
 data "http" "volterra_get_blocked_by_waf" {
   provider = http-full
 
-  url = var.api_get_security_events_url
+  url    = var.api_get_security_events_url
   method = "POST"
   request_headers = {
-    Content-Type = "application/json"
+    Content-Type  = "application/json"
     Authorization = format("APIToken %s", var.api_token)
   }
 
-  request_body = jsonencode({ aggs: {}, end_time: var.end_timestamp, limit: 0, namespace: "wsronek-ns1", query: "{authority=\"lloyds-boutique.acmecorp-stage.f5xc.app\",sec_event_type=\"waf_sec_event\"}",scroll: false, sort: "DESCENDING", start_time: var.start_timestamp })
+  request_body = jsonencode({ aggs : {}, end_time : var.end_timestamp, limit : 0, namespace : "wsronek-ns1", query : "{authority=\"lloyds-boutique.acmecorp-stage.f5xc.app\",sec_event_type=\"waf_sec_event\"}", scroll : false, sort : "DESCENDING", start_time : var.start_timestamp })
 }
 
 data "jq_query" "json_parser" {
-  depends_on = [ data.http.volterra_get_blocked_by_waf ]
+  depends_on = [data.http.volterra_get_blocked_by_waf]
 
-  data = data.http.volterra_get_blocked_by_waf.body
+  data  = data.http.volterra_get_blocked_by_waf.body
   query = "[.events[] | fromjson | select( .signatures != {} ) | { signature_id: .signatures[].id, method: .method, path: .req_path, host: .authority, params: .req_params } ] | unique"
 }
 
 resource "local_file" "signatures" {
-    content  = format("variable \"waf_exclusion_rules\" {\n  type = set( object( {\n    name = string\n    signature_id = string\n    method = string\n    host = string\n    path = string\n    params = string\n  } ) )\n  default = %s\n}", data.jq_query.json_parser.result)
-    filename = "waf_exclusion_rules_defined_within_interval.tf"
+  content  = format("variable \"waf_exclusion_rules\" {\n  type = set( object( {\n    name = string\n    signature_id = string\n    method = string\n    host = string\n    path = string\n    params = string\n  } ) )\n  default = %s\n}", data.jq_query.json_parser.result)
+  filename = "waf_exclusion_rules_defined_within_interval.tf"
 }
