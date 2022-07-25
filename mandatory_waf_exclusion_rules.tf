266ed7161af83c16513afd3f9f8ea2904de51ba3
Stephen Archer <s.archer@f5.com>
2022-07-25 18:30:17 +0100

    test run 10

--- mandatory_waf_exclusion_rules.tf
+++ mandatory_waf_exclusion_rules.tf
@@ -5,5 +5,5 @@ variable "mandatory_waf_exclusion_rules" {
     host = string
     path = string
   } ) )
-  default = [{"host":"juice.gal.volterra.link","method":"GET","path":"/rest/etc/passwd","signature_id":"200010468"},{"host":"juice.gal.volterra.link","method":"GET","path":"/rest/products/1/reviews","signature_id":"200002053"}]
+  default = []
 }
