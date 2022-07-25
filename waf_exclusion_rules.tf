8c918235406e1e01a5766815a27e1aab22213ca7
Stephen Archer <s.archer@f5.com>
2022-07-25 18:12:12 +0100

    test run 10

--- waf_exclusion_rules.tf
+++ waf_exclusion_rules.tf
@@ -39,6 +39,7 @@ data "jq_query" "json_parser" {
 output "jquery" {
   value = data.jq_query.json_parser.result
 }
+
 output "timestamps" {
   value = format("timestamp_start: %s, timestamp_end: %s, request body:%s, response body:%s ", var.timestamp_start, var.timestamp_end, data.http.volterra_get_blocked_by_waf.request_body, data.http.volterra_get_blocked_by_waf.body)
 }
