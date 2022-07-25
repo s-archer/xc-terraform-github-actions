65390287c764b40b6b1795ddbfba111d331161dd
Stephen Archer <s.archer@f5.com>
2022-07-25 15:18:40 +0100

    updated curl tests to use volterra.link domain [4]

--- waf_exclusion_rules/main.tf
+++ waf_exclusion_rules/main.tf
@@ -65,9 +65,4 @@ data "jq_query" "json_parser" {
 resource "local_file" "waf_exclusion_rules_defined_within_interval" {
   content  = format("variable \"waf_exclusion_rules\" {\n  type = set( object( {\n    signature_id = string\n    method = string\n    host = string\n    path = string\n } ) )\n  default = %s\n}", data.jq_query.json_parser.result)
   filename = "waf_exclusion_rules_defined_within_interval.tf"
-<<<<<<< HEAD
 }
-=======
-}
-
->>>>>>> bfcc160c27a963d2d420cc9940410f43415a22f7
