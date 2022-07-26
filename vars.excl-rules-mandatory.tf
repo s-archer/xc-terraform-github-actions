87acce082ed4483b805c365bf77e2ad678c29e27
Stephen Archer <s.archer@f5.com>
2022-07-26 22:14:39 +0100

    demo run - removed mandatory rule

--- vars.excl-rules-mandatory.tf
+++ vars.excl-rules-mandatory.tf
@@ -5,5 +5,5 @@ variable "waf_exclusion_rules_mandatory" {
     host         = string
     path         = string
   }))
-  default = [{"host":"juice.gal.volterra.link","method":"GET","path":"/rest/products/1/reviews","signature_id":"200002053"}]
+  default = []
 }
