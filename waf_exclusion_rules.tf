e9a172a334329439d80f3899d24f7e925d8efc36
Stephen Archer <s.archer@f5.com>
2023-01-13 09:50:05 +0000

    replaced galileo tenant with emea-ent

--- waf_exclusion_rules.tf
+++ waf_exclusion_rules.tf
@@ -7,9 +7,9 @@ resource "volterra_api_credential" "api" {
     when    = destroy
     command = <<-EOF
       #!/bin/bash
-      NAME=$(curl --location --request GET 'https://galileo.console.ves.volterra.io/api/web/namespaces/system/api_credentials' \
+      NAME=$(curl --location --request GET 'https://f5-emea-ent.console.ves.volterra.io/api/web/namespaces/system/api_credentials' \
         --header 'Authorization: APIToken ${self.data}'| jq 'first(.items[] | select (.name | contains("${self.name}")) | .name)') 
-      curl --location --request POST 'https://galileo.console.ves.volterra.io/api/web/namespaces/system/revoke/api_credentials' \
+      curl --location --request POST 'https://f5-emea-ent.console.ves.volterra.io/api/web/namespaces/system/revoke/api_credentials' \
         --header 'Authorization: APIToken ${self.data}' \
         --header 'Content-Type: application/json' \
         -d "$(jq -n --arg n "$NAME" '{"name": $n, "namespace": "system" }')"
