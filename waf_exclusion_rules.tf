5b09dee9ae70177ffea5af021e7cf02690859c01
Stephen Archer <s.archer@f5.com>
2025-05-27 20:24:00 +0100

    updated jq

--- waf_exclusion_rules.tf
+++ waf_exclusion_rules.tf
@@ -24,7 +24,37 @@ data "jq_query" "json_parser" {
   depends_on = [data.http.volterra_get_blocked_by_waf]
 
   data  = data.http.volterra_get_blocked_by_waf.response_body
-  query = "[.events[] | fromjson | select( .signatures != {} ) | { signature_id: .signatures[].id, method: .method, path: .req_path, host: .authority, context: .signatures[].context } ] | unique"
+  # query = "[.events[] | fromjson | select( .signatures != {} ) | { signature_id: .signatures[].id, method: .method, path: .req_path, host: .authority, context: .signatures[].context } ] | unique"
+  query = <<EOT
+[
+  .events[] 
+  | fromjson 
+  | select(.signatures != {}) 
+  | .signatures[] 
+  | {
+      signature_id: .id,
+      method: .method,
+      path: .req_path,
+      host: .authority,
+      context: (
+        if .context | test("^parameter") then "CONTEXT_PARAMETER"
+        elif .context | test("^url") then "CONTEXT_URL"
+        elif .context | test("^cookie") then "CONTEXT_COOKIE"
+        else .context
+        end
+      ),
+      context_name: (
+        if .context | test("^parameter \\(") then
+          (.context | capture("^parameter \\((?<val>[^\\s)]+)") | .val)
+        elif .context | test("^cookie \\(") then
+          (.context | capture("^cookie \\((?<val>[^\\s)]+)") | .val)
+        else
+          empty
+        end
+      )
+    }
+] | unique
+EOT
 }
 
 resource "local_file" "waf_exclusion_rules_defined_within_interval" {
