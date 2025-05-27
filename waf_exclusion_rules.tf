125127c657c9a3224cf2e646e261bdb863d09646
Stephen Archer <s.archer@f5.com>
2025-05-27 19:32:37 +0100

    updated waf query

--- waf_exclusion_rules.tf
+++ waf_exclusion_rules.tf
@@ -1,22 +1,3 @@
-
-# resource "volterra_api_credential" "api" {
-#   name                = format("%s-api-token", var.shortname)
-#   api_credential_type = "API_TOKEN"
-
-#   provisioner "local-exec" {
-#     when    = destroy
-#     command = <<-EOF
-#       #!/bin/bash
-#       NAME=$(curl --location --request GET 'https://f5-emea-ent.console.ves.volterra.io/api/web/namespaces/system/api_credentials' \
-#         --header 'Authorization: APIToken ${self.data}'| jq 'first(.items[] | select (.name | contains("${self.name}")) | .name)') 
-#       curl --location --request POST 'https://f5-emea-ent.console.ves.volterra.io/api/web/namespaces/system/revoke/api_credentials' \
-#         --header 'Authorization: APIToken ${self.data}' \
-#         --header 'Content-Type: application/json' \
-#         -d "$(jq -n --arg n "$NAME" '{"name": $n, "namespace": "system" }')"
-#     EOF
-#   }
-# }
-
 resource "volterra_api_credential" "api" {
   name                = format("%s-api-token", var.shortname)
   api_credential_type = "API_TOKEN"
@@ -43,7 +24,7 @@ data "jq_query" "json_parser" {
   depends_on = [data.http.volterra_get_blocked_by_waf]
 
   data  = data.http.volterra_get_blocked_by_waf.response_body
-  query = "[.events[] | fromjson | select( .signatures != {} ) | { signature_id: .signatures[].id, method: .method, path: .req_path, host: .authority } ] | unique"
+  query = "[.events[] | fromjson | select( .signatures != {} ) | { signature_id: .signatures[].id, method: .method, path: .req_path, host: .authority, context: .context } ] | unique"
 }
 
 resource "local_file" "waf_exclusion_rules_defined_within_interval" {
